4adf0a882baee460ad71466711f55252e0f95e98
khmiri007 <khmirizied05@gmail.com>
2017-05-19 22:50:04 +0200

    Initial commit

---
+++ app/src/main/java/ziedkhmiri/custom_layout_3d/MainActivity.java
@@ -0,0 +1,13 @@
+package ziedkhmiri.custom_layout_3d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
